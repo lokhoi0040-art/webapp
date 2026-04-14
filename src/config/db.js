3e6cf7061644cc6a7aedc3b91101dc6bc6c4e77d
lokhoi <emailcuaban@gmail.com>
2026-04-14 14:03:50 +0700

    fix db using DATABASE_URL

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,5 @@
 const mysql = require("mysql2/promise");
 
-const db = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT || 3306,
-});
+const db = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = db;
