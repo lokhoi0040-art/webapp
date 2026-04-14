a409b1cc74ec54d6591c57689c6d170d2974f513
lokhoi <emailcuaban@gmail.com>
2026-04-14 14:12:50 +0700

    fix db config

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,13 @@
 const mysql = require("mysql2/promise");
 
-const db = mysql.createPool(process.env.DATABASE_URL);
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST || "mysql.railway.internal",
+  user: process.env.MYSQLUSER || "root",
+  password: process.env.MYSQLPASSWORD || "",
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
 
-module.exports = db;
+module.exports = pool;
