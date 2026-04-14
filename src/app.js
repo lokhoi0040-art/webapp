1b36add93dc4c362274bc4d0077597ffec07acd5
lokhoi <emailcuaban@gmail.com>
2026-04-14 09:46:31 +0700

    fix port for railway

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ app.get("/", (req, res) => {
   res.send("API đang chạy 🚀");
 });
 
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
 });
