37acddb8dc8bb2007c379879e42f91173f9fb567
lokhoi <emailcuaban@gmail.com>
2026-04-14 12:21:44 +0700

    fix db config

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,11 @@
 const mysql = require("mysql2/promise");
 
-const db = mysql.createPool(process.env.MYSQL_URL);
-
-console.log("✅ DB Pool created");
+const db = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306,
+});
 
 module.exports = db;
