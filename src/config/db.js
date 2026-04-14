0fb27fcd0ec9da132fa750bb9e620e38ad1d8b17
lokhoi <emailcuaban@gmail.com>
2026-04-14 11:48:54 +0700

    fix mysql connection

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ const db = mysql.createPool({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  port: Number(process.env.MYSQLPORT) // 🔥 FIX
 });
 
 console.log("✅ DB Pool created");
